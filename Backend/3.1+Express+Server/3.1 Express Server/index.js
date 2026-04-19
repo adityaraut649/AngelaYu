84f2468e2a5e86ec7f739da46ce8b56549ede784
adityaraut649 <vraut3468@gmail.com>
2026-04-19 12:57:00 +0530

    Https

---
+++ Backend/3.1+Express+Server/3.1 Express Server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
+
+
+
+
+// Creating an Express Server
+// 1. Create directory for your project
+// 2. create a index.js file
+// 3. Initialize NPM
+// 4. Install Express
+// 5. Create a server.js file
+// 6. Create a route
+
+
